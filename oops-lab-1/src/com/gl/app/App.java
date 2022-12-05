8717d9fcade57646aec16b11f35b3501bcb6741e
ayan-neel <116948588+ayan-neel@users.noreply.github.com>
2022-12-05 09:48:04 +0530

    added oops lab 1 project

---
+++ oops-lab-1/src/com/gl/app/App.java
@@ -0,0 +1,54 @@
+package com.gl.app;
+
+import java.util.Scanner;
+
+import com.gl.model.Employee;
+import com.gl.service.CredentialService;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String department=null;
+		Employee emp = new Employee("Ayan","Medhi");
+		CredentialService cs = new CredentialService();
+		boolean flag=true;
+		while(flag) {
+			System.out.println("Please enter the department from the following: \n1. Technical\n2. Admin\n3. Legal\n4. Human Resource");
+			int choice = sc.nextInt();
+			switch(choice)
+			{
+				case 1:
+					department="Technical";
+					flag=false;
+					break;
+				case 2:
+					department="Admin";
+					flag=false;
+					break;
+				case 3:
+					department="Legal";
+					flag=false;
+					break;
+				case 4:
+					department="Human Resource";
+					flag=false;
+					break;
+				default:
+					System.out.println("Invalid option! please enter correct choice");
+					break;
+			}
+		}
+		String emailId = cs.generateEmailAddress(emp.getFirstName(),emp.getLastName(),department);
+		String password = cs.generatePassword();
+		String message="Dear %s your generated credentials are as follows\nEmail-----> %s\nPassword---->%s";
+		String formattedMessage = String.format(message, emp.getFirstName(),emailId,password);
+		System.out.println(formattedMessage);
+		
+		
+		
+
+	}
+
+}
---
+++ oops-lab-1/src/com/gl/service/CredentialService.java
@@ -0,0 +1,46 @@
+package com.gl.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class CredentialService {
+	
+	private  ArrayList<List<String>>passwordBucket= new ArrayList<List<String>>();
+	
+	public CredentialService()
+	{
+		passwordBucket.add(Arrays.asList("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"));
+		passwordBucket.add(Arrays.asList("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"));
+		passwordBucket.add(Arrays.asList("1","2","3","4","5","6","7","8","9","0"));
+		passwordBucket.add(Arrays.asList("@","#","$","&"));
+		
+	}
+	public  String generatePassword()
+	{
+		String  password="";
+		Random random = new Random();
+		for(int i=0;i<8;i++) {
+			List<String> selectedBucket=passwordBucket.get(random.nextInt(0, passwordBucket.size()));
+			password+=selectedBucket.get(random.nextInt(0,selectedBucket.size()));	
+		}
+		return password;
+			
+	}
+	public String generateEmailAddress(String firstName,String lastName,String department)
+	{
+		String emailId="";
+		emailId+=firstName+"."+lastName+"@"+department+"."+"abc.com";
+		emailId=emailId.toLowerCase();
+		return emailId;
+		
+		
+	}
+	public static void main(String [] args)
+	{
+		CredentialService cs = new CredentialService();
+		System.out.println("Generated email: "+cs.generateEmailAddress("Ayan","Medhi", "Technical"));
+		System.out.println("Generated password: "+cs.generatePassword());
+	}
+}
